224f0bbd6b21aff81b9450d69bf5a6f77c0b7ade
Athul Dinesan <athuldinesan5@gmail.com>
2026-01-01 13:10:02 +0530

    feat: add support for duckduckgo and mobile support
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- README.md
+++ README.md
@@ -5,19 +5,19 @@
 </h1>
 
 <p align="center">
-  <strong>Adds the stremio button to watch now instead of the other streaming service to open up stremio directly from google search
+  <strong>Adds the stremio button to watch now instead of the other streaming service to open up stremio directly from google/duckduckgo search
 </strong>
 </p>
+
+## Features
+* Supported Search Engines : Google, Duckduckgo
+* Mobile support for firefox (chromium based browsers don't have extension support)
+* Floating stremio icon in mobile if watch options section is not available.
 
 ## Installation
 ### Chrome - (Any chromium based browsers like brave,edge etc.)
 * Goto Chrome and Click on customise at top right corner > Extension > Manage Extensions
 * Enable Developer Mode
-#### Method 1 - (Recommended)
-* Download the **chrome-stremio-search.crx** file from the [Releases Tab](https://github.com/athuld/stremio-google-search-extension/releases/tag/latest)
-* Drag and drop the crx file to the manage extensions window
-* Click on add extension and click on trust extension on the extension review option that comes up (look at video if confused)
-#### Method 2
 * Download the zip file for the project and extract it. [Download](https://github.com/athuld/stremio-google-search-extension/archive/refs/heads/main.zip)
 * Click on Load unpacked and select the **chrome** folder inside the extracted folder
 * You should now see the extension installed. Browse and Enjoy :)
@@ -26,13 +26,19 @@
 <p><a target="_blank" href="https://addons.mozilla.org/en-US/firefox/addon/stremio-google-search/" title="Stremio Google Search"><img src="assets/firefoxaddon.png" width="200"/></a></p>
 
 ## Video Tutorial
-### Chrome Method 1
-![](assets/stremio-search-crx.gif)
-### Chrome Method 2
+### Chrome
 ![](assets/stremio-seach-extension.gif)
 
-## Screenshot
+## Donate
+* If anyone wants to help out publishing the extension in chrome store and also suppport.
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/athuldinesan_)
+
+## Screenshots
 ![](assets/pic-selected-250823-1711-34.png)
+![](assets/duckduckgo.png)
+![](assets/firefox-google-mob.jpg)
+![](assets/firefox-duck-mob.jpg)
 
 ## Notes
 * I made this for personal use since i always search my movies and series first and then watch it after checking reviews and scores etc..
--- chrome/scripts/script.js
+++ chrome/scripts/script.js
@@ -1,81 +1,264 @@
+/***********************************************************************/
+/*    Common Helper Functions
+/***********************************************************************/
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+const isFirefox = typeof browser !== "undefined";
+const IMAGE_PATH = "images/stremio-logo-small.png";
+const STREMIO_IMG = isFirefox ? browser.runtime.getURL(IMAGE_PATH) : chrome.runtime.getURL(IMAGE_PATH);
+const THEME = document.querySelector("meta[content='dark light']")
+  ? "dark"
+  : "light";
+
+/* Function to get imdb code from DOM */
+const getImdbCodeFromDom = () => {
+  let imdbLink =
+    document.querySelector("a[href*='https://www.imdb.com/title']")?.href ||
+    document.querySelector("a[href*='https://m.imdb.com/title']")?.href;
+  imdbCode = imdbLink?.match(/title\/(tt\d+)/)?.[1];
+  return imdbCode || null;
+};
+
+/* Function to add a long press event-listener to remove stremio icon */
+const addLongPressListener = (fabStremio) => {
+  let pressTimer;
+  let isLongPress = false;
+
+  const handleLongPress = (e) => {
+    isLongPress = true;
+    link.style.display = "none";
+  };
+
+  const start = (e) => {
+    if (e.type === "click" && e.button !== 0) return;
+    isLongPress = false;
+
+    pressTimer = setTimeout(() => {
+      handleLongPress(e);
+    }, 1000);
+  };
+
+  const cancel = (e) => {
+    if (pressTimer !== null) {
+      clearTimeout(pressTimer);
+      pressTimer = null;
+    }
+  };
+
+  fabStremio.addEventListener("click", (e) => {
+    if (isLongPress) {
+      e.preventDefault();
+    }
+  });
+
+  fabStremio.addEventListener("mousedown", start);
+  fabStremio.addEventListener("mouseup", cancel);
+  fabStremio.addEventListener("mouseleave", cancel);
+  fabStremio.addEventListener("touchstart", start, { passive: true });
+  fabStremio.addEventListener("touchend", cancel);
+  fabStremio.addEventListener("touchcancel", cancel);
+
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 100) {
+      fabStremio.classList.add("fab-hidden");
+    } else {
+      fabStremio.classList.remove("fab-hidden");
+    }
+    lastScrollY = currentScrollY;
+  });
+};
+
+/* Function to add Common Floation action button to search */
+const addFabStremio = (contentType, imdbCode) => {
+  const fabStremio = document.createElement("a");
+  fabStremio.id = "floating-btn";
+  fabStremio.href = `stremio:///detail/${contentType}/${imdbCode}`;
+  fabStremio.innerHTML = `<img style='width: 30px;height: 30px;' src=${STREMIO_IMG} />`;
+  document.body.appendChild(fabStremio);
+  addLongPressListener(fabStremio);
+  return;
+};
+
+/*****************************************************************/
+/*    Search Engine: Google Specific Functions
+/*****************************************************************/
+
+// Function to work with mobile view
+const addMobileStremioGoogle = (imdbCode, contentType) => {
+  const watchNowMain = document.querySelector(
+    "div[role='list'][data-ved][lang]",
+  );
+  const stremioHref = `stremio:///detail/${contentType}/${imdbCode}`;
+  try {
+    if (watchNowMain) {
+      const mainTag = watchNowMain.querySelector(
+        "a[ping]",
+      );
+      if (mainTag) {
+        mainTag.querySelector("img").src = STREMIO_IMG;
+        mainTag.href = stremioHref;
+      }
+      const secTagContainer = watchNowMain.lastChild;
+      let firstTag = secTagContainer.querySelector(
+        "a[ping]",
+      );
+      let firstTagDiv = firstTag?.parentElement;
+      if (firstTagDiv) {
+        firstTagDiv.querySelector("img").src = STREMIO_IMG;
+        firstTagDiv.querySelector("a").href = stremioHref;
+        let textSection = firstTagDiv.firstChild.querySelector("div:nth-child(2)");
+        textSection.firstChild.innerHTML = "Stremio";
+        textSection.lastChild.innerHTML = "Freedom to stream";
+        return;
+      }
+    }
+    // Fallback to FAB if all the above doesn't work
+    addFabStremio(contentType, imdbCode);
+  } catch (error) {
+    addFabStremio(contentType, imdbCode);
+  }
+};
+
+// Main function for Google implementation
 const addStremioButtonToGoogle = () => {
-  let seriesOptions = document.querySelector(
+  let seriesOptions = document.querySelectorAll(
     "div[data-attrid='kc:/tv/tv_program:media_actions_wholepage']",
   );
-  let movieOptions = document.querySelector(
+  let movieOptions = document.querySelectorAll(
     "div[data-attrid='kc:/film/film:media_actions_wholepage']",
   );
   let filmReviewContainer = document.querySelector(
-    "div[data-attrid='kc:/film/film:reviews']",
+    "div[data-attrid^='kc:/film/film:']",
   );
   let seriesReviewContainer = document.querySelector(
-    "div[data-attrid='kc:/tv/tv_program:reviews']",
+    "div[data-attrid^='kc:/tv/tv_program:']",
   );
 
-  let watchOption = null;
+  let watchOptions = [];
   let reviewContainer = null;
   let isMovie = true;
   let contentType = "movie";
   let imdbCode = null;
 
-  if (seriesOptions) {
-    watchOption = seriesOptions;
+  if (seriesReviewContainer != null) {
     reviewContainer = seriesReviewContainer;
     isMovie = false;
     contentType = "series";
-  } else if (movieOptions) {
-    watchOption = movieOptions;
+  } else if (filmReviewContainer != null) {
     reviewContainer = filmReviewContainer;
   }
 
-  if (watchOption === null) {
+  if (seriesOptions.length !== 0) {
+    watchOptions = [...seriesOptions];
+  } else if (movieOptions.length !== 0) {
+    watchOptions = [...movieOptions];
+  }
+
+  // Get imdb code from DOM
+  imdbCode = getImdbCodeFromDom();
+
+  if (imdbCode === null) {
     return;
   }
 
-  if (reviewContainer != null) {
-    let imdbEle = reviewContainer.querySelector(
-      "a[href*='https://www.imdb.com/']",
-    );
+  if (isMobile) {
+    addMobileStremioGoogle(imdbCode, contentType);
+    return;
+  }
 
-    if (imdbEle) {
-      let imdbParts = imdbEle.href.split("/");
-      imdbCode = imdbParts.pop() || imdbParts.pop();
+  if (reviewContainer !== null && watchOptions.length === 0) {
+    addFabStremio(contentType, imdbCode);
+    return;
+  }
+
+  watchOptions.forEach((watchOption) => {
+    let childCount =
+      watchOption.firstElementChild.firstElementChild.childElementCount;
+
+    let watchNowEle =
+      watchOption.firstElementChild.firstElementChild.firstElementChild;
+
+    if (childCount === 2) {
+      let divEle = document.createElement("div");
+      watchNowEle =
+        watchOptions.firstElementChild.firstElementChild.insertBefore(
+          divEle,
+          watchNowEle,
+        );
     }
-  }
-
-  if (imdbCode === null) {
-    /* Add a fallback to imdb code as some of the other languages doesn't habe imdb in the reviewContainer */
-    let imdbLink = document.querySelector(
-      "a[href*='https://www.imdb.com/']",
-    )?.href;
-    imdbCode = imdbLink?.match(/title\/(tt\d+)/)?.[1];
-  }
-
-  if (imdbCode === null) {
-    return;
-  }
-
-  let childCount =
-    watchOption.firstElementChild.firstElementChild.childElementCount;
-
-  let watchNowEle =
-    watchOption.firstElementChild.firstElementChild.firstElementChild;
-
-  if (childCount === 2) {
-    let divEle = document.createElement("div");
-    watchNowEle = watchOption.firstElementChild.firstElementChild.insertBefore(
-      divEle,
-      watchNowEle,
-    );
-  }
-
-  watchNowEle.innerHTML = `<a class="stremio-cta__href" href='stremio:///detail/${contentType}/${imdbCode}'>
-        <div class="streamio-cta__container">
-          <img style='width: 40px;height: 40px;' src='https://www.stremio.com/website/stremio-logo-small.png'/>
-          <div>Stremio</div>
-          <div>Freedom to stream</div>
-        </div>
-      </a>`;
+    watchNowEle.innerHTML = `<a class="stremio-cta__href" href='stremio:///detail/${contentType}/${imdbCode}'>
+         <div class="streamio-cta__container">
+         <img style='width: 40px;height: 40px;' src=${STREMIO_IMG} />
+         <div>Stremio</div>
+         <div>Freedom to stream</div>
+         </div>
+         </a>`;
+  });
 };
 
-addStremioButtonToGoogle();
+/*******************************************************/
+/* Search Engine: DuckDuckGo Specific Functions
+/*******************************************************/
+const addStremioButtonToDuckDuckGo = () => {
+  const titleSection = document.querySelector(
+    'div[data-react-module-id="titles"]',
+  );
+  if (!titleSection) return;
+  imdbCode = getImdbCodeFromDom();
+  if (imdbCode === null) {
+    return;
+  }
+  contentType =
+    titleSection.innerText.includes("TV Series") ||
+    titleSection.innerText.includes("television series")
+      ? "series"
+      : "movie";
+  try {
+    let xpath = "//span[text()='Quick links']";
+    let quickLinksSpan = document.evaluate(
+      xpath,
+      titleSection,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null,
+    ).singleNodeValue;
+
+    // Add stremio to that imdb card if available
+    if (quickLinksSpan) {
+      let quickLinksDiv = quickLinksSpan.parentElement.parentElement.lastChild;
+      if (
+        quickLinksDiv.innerHTML.includes("imdb") &&
+        quickLinksDiv.childElementCount <= 7
+      ) {
+        let stremioLinkCopy = quickLinksDiv.firstElementChild.cloneNode(true);
+        stremioLinkCopy.href = `stremio:///detail/${contentType}/${imdbCode}`;
+        stremioLinkCopy.querySelector("img").src = STREMIO_IMG;
+        stremioLinkCopy.querySelector("span").innerText = "Stremio";
+        quickLinksDiv.insertBefore(stremioLinkCopy, quickLinksDiv.firstChild);
+        return;
+      }
+    }
+    // Fallback to add floating action button if imdb card is not available
+    addFabStremio(contentType, imdbCode);
+  } catch (error) {
+    addFabStremio(contentType, imdbCode);
+  }
+};
+
+const searchEngine = window.location.hostname;
+if (searchEngine.includes("google.")) {
+  addStremioButtonToGoogle();
+} else if (searchEngine.includes("duckduckgo.")) {
+  document.addEventListener(
+    "animationstart",
+    function (event) {
+      if (event.animationName === "quickLinkAppeared") {
+        addStremioButtonToDuckDuckGo();
+      }
+    },
+    true,
+  );
+}
